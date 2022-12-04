748e995e3b96a0c1e3392e6a35bacd83cf0f4244
Sergio Sosa Bautista <sergioasb8@gmail.com>
2022-12-03 21:31:40 -0500

    col-spanish V. 0.0.2

--- col-spanish/tests/test_col_spanish.py
+++ col-spanish/tests/test_col_spanish.py
@@ -2,7 +2,7 @@ from col_spanish import __version__
 from col_spanish import del_punctuation, del_accents
 
 def test_version():
-    assert __version__ == '0.0.1'
+    assert __version__ == '0.0.2'
 
 def test_del_punctuation():
     text = "Hola, como estas? bien!"
