4994fccd29cf17eb4eb223e63912cfd7893b367c
Sergio Sosa Bautista <sergioasb8@gmail.com>
2022-12-07 14:17:43 -0500

    add setup.py v. 0.0.2

--- col-spanish/col_spanish/__init__.py
+++ col-spanish/col_spanish/__init__.py
@@ -1,3 +1,9 @@
-__version__ = '0.0.2'
+__title__ = "colspanish"
+__version__ = "0.0.2"
+__author__ = "Sergio A. Sosa Bautista"
+__email__ = "sergio.sosa.py8@gmail.com"
+__description__ = "A collection of tools to work with text write in Spanish (Colombia)"
+__url__ = "https://github.com/sergioasb8/col-spanish"
+__license__ = "MIT License"
 
 from .col_spanish import *
