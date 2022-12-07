25f8a66f86e24b418453d0e898aae62de5f942d3
Sergio Sosa Bautista <sergioasb8@gmail.com>
2022-12-07 15:03:52 -0500

    create out setup.py

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+import re
+import codecs
+
+NAME = "col_spanish"
+META_PATH = os.path.join(NAME,NAME, "__init__.py")
+REQUIREMENTS = []
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independet",
+    "Programming Language :: Python",
+]
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and return the contents  of the resulting file. Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+META_FILE =read(META_PATH)
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta), META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+if __name__ == "__main__":
+
+    setup(
+        name = "col_spanish",
+        decription = find_meta("description"),
+        license = find_meta("license"),
+        url = find_meta("url"),
+        version = find_meta("version"),
+        author = find_meta("author"),
+        email = find_meta("email"),
+        long_description = open("README.md").read(),
+        packages = find_packages(),
+        zip_safe = False,
+        install_requires = [],
+        classifiers = CLASSIFIERS,
+    )
